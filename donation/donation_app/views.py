77d60218a26978d7555d5ac3eedd34cf43191edd
MarcinLebiedzinski <lebiedzinski.marcin@gmail.com>
2023-04-15 10:59:47 +0200

    Update 2023-04-15

--- donation/donation_app/views.py
+++ donation/donation_app/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
 
 # Create your views here.
+
+
+class LandingPage(View):
+    def get(self, request):
+        ctx = {'data': "sampledata"}
+        return render(request, 'index.html', ctx)
+
+
+class AddDonation(View):
+    def get(self, request):
+        ctx = {'data': "sampledata"}
+        return render(request, 'form.html', ctx)
+
+
+class Login(View):
+    def get(self, request):
+        ctx = {'data': "sampledata"}
+        return render(request, 'login.html', ctx)
+
+
+class Register(View):
+    def get(self, request):
+        ctx = {'data': "sampledata"}
+        return render(request, 'register.html', ctx)
