a9bed0e8c08e72390bae574a90c28de010325d44
MarcinLebiedzinski <lebiedzinski.marcin@gmail.com>
2023-04-17 16:23:02 +0200

    features_0-2 for aproval

--- donation/donation_app/views.py
+++ donation/donation_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from .models import Category, Institution, Donation
-
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -16,8 +16,36 @@ class LandingPage(View):
             if donation.institution not in list_of_institutions:
                 list_of_institutions.append(donation.institution)
         amount_of_institutions = len(list_of_institutions)
+
+        institutions = Institution.objects.all()
+        foundations_list = []
+        corporates_list = []
+        individuals_list = []
+        for institution in institutions:
+            categories = Category.objects.filter(institution__id=institution.id)
+            categories_names = ', '.join([category.name for category in categories])
+            if institution.type == 0:
+                foundations_list.append((institution, categories_names))
+            elif institution.type == 1:
+                corporates_list.append((institution, categories_names))
+            elif institution.type == 2:
+                individuals_list.append((institution, categories_names))
+
+        foundation_paginator = Paginator(foundations_list, 1)
+        corporate_paginator = Paginator(corporates_list, 1)
+        individual_paginator = Paginator(individuals_list, 1)
+
+        page = request.GET.get('page')
+        foundations = foundation_paginator.get_page(page)
+        corporates = corporate_paginator.get_page(page)
+        individuals = individual_paginator.get_page(page)
+
         ctx = {'amount_of_institutions': amount_of_institutions,
-               'quantity_of_donations': quantity_of_donations}
+               'quantity_of_donations': quantity_of_donations,
+               'foundations': foundations,
+               'corporates': corporates,
+               'individuals': individuals
+               }
         return render(request, 'index.html', ctx)
 
 
